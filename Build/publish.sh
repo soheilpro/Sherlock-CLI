320df8ecf0e84ac70c9850e2db5d4767657c88d9
Soheil Rashidi <contact@soheilrashidi.com>
2020-01-11 01:19:11 +0800

    Single file executable.

--- Build/publish.sh
+++ Build/publish.sh
@@ -8,7 +8,7 @@ function _publish {
   rm -rf "$DIR/../Publish/$PLATFORM"
   mkdir -p "$DIR/../Publish/$PLATFORM"
 
-  dotnet publish "$DIR/../Sources/Sherlock/Sherlock.csproj" --configuration Release --runtime $PLATFORM --output "$DIR/../Publish/$PLATFORM"
+  dotnet publish "$DIR/../Sources/Sherlock/Sherlock.csproj" --configuration Release --runtime $PLATFORM --output "$DIR/../Publish/$PLATFORM" /p:PublishSingleFile=true /p:PublishTrimmed=true
 }
 
 _publish linux-x64
